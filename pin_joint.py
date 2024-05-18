da1a7ab5533bb80bbff5a03105629172ac984a9b
Kanghyun Lee <strongwire2@gmail.com>
2024-05-18 14:08:45 +0900

    pin joint

--- pin_joint.py
+++ pin_joint.py
@@ -12,10 +12,9 @@ circle = pymunk.Circle(body, radius=20)
 joint = pymunk.constraints.PinJoint(b0, body, (200, 200))
 
 body2 = pymunk.Body(mass=1, moment=10)
-body2.position = 400, 100
+body2.position = 240, 340
 circle2 = pymunk.Circle(body2, radius=20)
-joint2 = pymunk.constraints.PinJoint(b0, body2, (300, 200))
+joint2 = pymunk.constraints.PinJoint(b0, body2, (240, 200))
 app.space.add(body, circle, joint, body2, circle2, joint2)
-
 
 app.run()
