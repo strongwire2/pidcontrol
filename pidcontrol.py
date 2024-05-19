e98877b72d57d80c71887ccbba421c8c048d6a3e
Kanghyun Lee <strongwire2@gmail.com>
2024-05-20 01:07:13 +0900

    중앙기준 시소 완성

--- pidcontrol.py
+++ pidcontrol.py
@@ -7,8 +7,8 @@ class PidApp:
     def __init__(self):
         self.seesaw_body = None
         self.seesaw = None
-        self.circle = None
-        self.body = None
+        self.ball_shape = None
+        self.ball_body = None
         self.seesaw: pymunk.Segment
 
         pygame.init()
@@ -27,37 +27,42 @@ class PidApp:
         self.space.gravity = 0, 1000
 
     def setup(self):
-        self.seesaw_body = pymunk.Body(body_type=pymunk.Body.KINEMATIC)
-        self.seesaw_body.position = 100, 400
-        self.seesaw = pymunk.Segment(self.seesaw_body, (100, 400), (700, 300), 1)
-        self.seesaw.elasticity = 0.5
-        self.seesaw.friction = 1
+        self.ball_body = pymunk.Body()
+        self.ball_body.position = 300, 200
+        self.ball_shape = pymunk.Circle(self.ball_body, radius=20)
+        self.ball_shape.elasticity = 1
+        self.ball_shape.friction = 1
+        self.ball_shape.density = 1  # 이거 안쓰면 에러남. 이걸 안쓰려면 Body 정의할 때 mass, inertia를 써주어야 함.
+        self.space.add(self.ball_body, self.ball_shape)
 
-        self.body = pymunk.Body()
-        self.body.position = 300, 200
-        self.circle = pymunk.Circle(self.body, radius=20)
-        self.circle.elasticity = 1
-        self.circle.friction = 1
-        self.circle.density = 1  # 이거 안쓰면 에러남. 이걸 안쓰려면 Body 정의할 때 mass, inertia를 써주어야 함.
-        self.space.add(self.body, self.circle, self.seesaw_body, self.seesaw)
+        # seesaw
+        self.arm_body = pymunk.Body()
+        self.arm_body.position = 400, 500
+        self.arm_shape0 = pymunk.Segment(self.arm_body, (-300, 0), (300, 0), 3)
+        self.arm_shape0.density = 1
+        self.arm_shape0.friction = 1
+        self.arm_shape1 = pymunk.Segment(self.arm_body, (-300, 0), (-300, -50), 3)
+        self.arm_shape1.density = 1
+        self.arm_shape1.friction = 1
+        self.arm_shape2 = pymunk.Segment(self.arm_body, (300, 0), (300, -50), 3)
+        self.arm_shape2.density = 1
+        self.arm_shape2.friction = 1
 
-        self.arm_body = pymunk.Body(10)
-        self.arm_body.position = 300, 300
-        self.arm_shape = pymunk.Segment(self.arm_body, (-100, 0), (100, 0), 1)
-        self.arm_shape.density = 1
-        self.space.add(self.arm_body, self.arm_shape)
+        self.space.add(self.arm_body, self.arm_shape0, self.arm_shape1, self.arm_shape2)
 
         self.fixed_body = pymunk.Body(body_type=pymunk.Body.KINEMATIC)
-        self.fixed_body.position = self.arm_body.position.x-100, self.arm_body.position.y
-        self.joint = pymunk.PinJoint(self.arm_body, self.fixed_body, (-100, 0), (0, 0))
+        self.fixed_body.position = self.arm_body.position.x, self.arm_body.position.y
+        self.joint = pymunk.PinJoint(self.arm_body, self.fixed_body, (0, 0), (0, 0))
         self.space.add(self.fixed_body, self.joint)
 
         self.upper_body = pymunk.Body(body_type=pymunk.Body.KINEMATIC)
-        self.upper_body.position = self.arm_body.position.x+100, self.arm_body.position.y-100
-        self.upper_joint = pymunk.PinJoint(self.arm_body, self.upper_body, (100, 0), (0, 0))
+        self.upper_body.position = self.arm_body.position.x+300, self.arm_body.position.y-200
+        self.upper_joint = pymunk.PinJoint(self.arm_body, self.upper_body, (300, -50), (0, 0))
         self.space.add(self.upper_body, self.upper_joint)
 
     def run(self):
+        offset = 0
+        delta = 1
         while self.running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -69,7 +74,12 @@ class PidApp:
             pygame.display.update()
             self.clock.tick(self.fps)  # 지정한 fps가 되도록 delay를 준다.
             self.space.step(1 / self.fps)  # 물리엔진은 1/FPS만큼 진행한다.
-            self.upper_body.position = (self.upper_body.position.x, self.upper_body.position.y+1)
+            if offset > 100:
+                delta = -1
+            elif offset < -100:
+                delta = 1
+            offset += delta
+            #self.upper_body.position = (self.upper_body.position.x, self.upper_body.position.y+delta)
         pygame.quit()
 
 
