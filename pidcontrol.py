57db9d7b33c32f51db36fc5d3f4cd28783e285e9
Kanghyun Lee <strongwire2@gmail.com>
2024-05-25 01:12:12 +0900

    class 제거

--- pidcontrol.py
+++ pidcontrol.py
@@ -2,97 +2,83 @@ import pymunk
 import pygame
 import pymunk.pygame_util
 
+pygame.init()
+size = (800, 800)  # 공간의 크기
+fps = 50
+clock = pygame.time.Clock()
 
-class PidApp:
-    def __init__(self):
-        self.seesaw_body = None
-        self.seesaw = None
-        self.ball_shape = None
-        self.ball_body = None
-        self.seesaw: pymunk.Segment
+space = pymunk.Space()  # 물리엔진 공간을 생성
+space.gravity = 0, 1000
 
-        pygame.init()
+screen = pygame.display.set_mode(size)  # 800x800 크기의 pygame 영역을 잡는다.
+# pygame과 pymunk의 좌표계가 다른걸 일치시킨다. pygame 기준으로 바꿈. 좌상단이 0, 0
+draw_options = pymunk.pygame_util.DrawOptions(screen)
 
-        self.segment = None
-        self.size = (800, 800)
-        self.fps = 50
-        self.clock = pygame.time.Clock()
+# Space에 놓을 객체들 정의
+# Ball Body와 Shape 정의
+ball_body = pymunk.Body()
+ball_body.position = 200, 200
+ball_shape = pymunk.Circle(ball_body, radius=20)  # ball_shape에 ball_body 연결함
+ball_shape.elasticity = 1
+ball_shape.friction = 1
+ball_shape.density = 1  # 이거 안쓰면 에러남. 이걸 안쓰려면 Body 정의할 때 mass, inertia를 써주어야 함.
+space.add(ball_body, ball_shape)
 
-        self.screen = pygame.display.set_mode(self.size)  # 800 x 800 크기의 영역을 잡는다
-        # pygame과 pymunk의 좌표계가 다른걸 일치시킨다. pygame 기준으로 바꿈. 좌상단이 0, 0
-        self.draw_options = pymunk.pygame_util.DrawOptions(self.screen)
-        self.running = True
+# Beam Body와 Beam Shape 정의
+# seesaw
+beam_body = pymunk.Body()
+beam_body.position = 400, 500
+beam_shape = pymunk.Segment(beam_body, (-300, 0), (300, 0), 3)
+beam_shape.density = 1
+beam_shape.friction = 1
+space.add(beam_body, beam_shape)
 
-        self.space = pymunk.Space()
-        self.space.gravity = 0, 1000
+# Beam Center 정의
+beam_center_body = pymunk.Body(body_type=pymunk.Body.KINEMATIC)
+beam_center_body.position = beam_body.position.x, beam_body.position.y
+beam_center_joint = pymunk.PinJoint(beam_body, beam_center_body, (0, 0), (0, 0))
+space.add(beam_center_body, beam_center_joint)
 
-    def setup(self):
-        self.ball_body = pymunk.Body()
-        self.ball_body.position = 200, 200
-        self.ball_shape = pymunk.Circle(self.ball_body, radius=20)
-        self.ball_shape.elasticity = 1
-        self.ball_shape.friction = 1
-        self.ball_shape.density = 1  # 이거 안쓰면 에러남. 이걸 안쓰려면 Body 정의할 때 mass, inertia를 써주어야 함.
-        self.space.add(self.ball_body, self.ball_shape)
+# Beam 오른쪽 매다는 handle 정의
+handle_body = pymunk.Body(body_type=pymunk.Body.KINEMATIC)
+handle_body.position = beam_body.position.x + 300, beam_body.position.y - 200
+handle_joint = pymunk.PinJoint(beam_body, handle_body, (300, 0), (0, 0))
+space.add(handle_body, handle_joint)
 
-        # seesaw
-        self.arm_body = pymunk.Body()
-        self.arm_body.position = 400, 500
-        self.arm_shape0 = pymunk.Segment(self.arm_body, (-300, 0), (300, 0), 3)
-        self.arm_shape0.density = 1
-        self.arm_shape0.friction = 1
-        self.arm_shape1 = pymunk.Segment(self.arm_body, (-300, 0), (-300, -50), 3)
-        self.arm_shape1.density = 1
-        self.arm_shape1.friction = 1
-        self.arm_shape2 = pymunk.Segment(self.arm_body, (300, 0), (300, -50), 3)
-        self.arm_shape2.density = 1
-        self.arm_shape2.friction = 1
+running = True
+prev_error = None
+set_point = 400  # 중앙을 타겟으로 함
 
-        self.space.add(self.arm_body, self.arm_shape0, self.arm_shape1, self.arm_shape2)
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    # 그림을 그린다
+    screen.fill((255, 255, 255))  # 흰색 배경
+    # debug_draw를 쓰면 직접 pygame에 그리지 않아도 됨. 알아서 그려줌
+    space.debug_draw(draw_options)
+    pygame.display.update()
+    clock.tick(fps)  # 지정한 fps가 되도록 delay를 준다.
+    space.step(1 / fps)  # 물리엔진은 1/FPS만큼 진행한다.
+    # 단순함을 위해 Error의 계산을 x 좌표로만 한다.
+    pv = ball_body.position.x  # pv = process value
+    error = set_point - pv  # set_point에서의 x 거리 차이를 error로 잡음
+    d_error = 0  # 오차의 변화를 담음
+    if prev_error is None:
+        d_error = 0
+    else:
+        d_error = error - prev_error
+    prev_error = error
+    # delta = e*0.3
+    delta = error*0.3 + d_error*30
+    print(f"e={error}, de={d_error} delta={delta}")
+    # 급발진하지 않도록 제한
+    if delta > 100:
+        delta = 100
+    elif delta < -100:
+        delta = -100
+    # 실제 process 수행
+    handle_body.position = (handle_body.position.x, 300 + delta)
 
-        self.fixed_body = pymunk.Body(body_type=pymunk.Body.KINEMATIC)
-        self.fixed_body.position = self.arm_body.position.x, self.arm_body.position.y
-        self.joint = pymunk.PinJoint(self.arm_body, self.fixed_body, (0, 0), (0, 0))
-        self.space.add(self.fixed_body, self.joint)
+pygame.quit()
 
-        self.upper_body = pymunk.Body(body_type=pymunk.Body.KINEMATIC)
-        self.upper_body.position = self.arm_body.position.x+300, self.arm_body.position.y-200
-        self.upper_joint = pymunk.PinJoint(self.arm_body, self.upper_body, (300, 0), (0, 0))
-        self.space.add(self.upper_body, self.upper_joint)
-
-    def run(self):
-        offset = 0
-        delta = 1
-        self.prev_e = None
-        while self.running:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.running = False
-                    # pygame.image.save(self.screen,'intro.png')
-            self.screen.fill((255, 255, 255))  # white background
-            # debug_draw를 쓰면 직접 pygame에 그리지 않아도 됨. 알아서 그려줌
-            self.space.debug_draw(self.draw_options)
-            pygame.display.update()
-            self.clock.tick(self.fps)  # 지정한 fps가 되도록 delay를 준다.
-            self.space.step(1 / self.fps)  # 물리엔진은 1/FPS만큼 진행한다.
-            #TODO: Error의 계산을 x 좌표로만 할게 아니고, 실제 ball의 중심을 기준으로 대각 기준을 잡아야 함.
-            e = 400 - self.ball_body.position.x
-            de = 0
-            if self.prev_e is None:
-                de = 0
-            else:
-                de = e - self.prev_e
-            self.prev_e = e
-            #delta = e*0.3
-            delta = e*0.3 + de*30
-            print(f"e={e}, de={de} delta={delta}")
-            if delta > 100:
-                delta = 100
-            elif delta < -100:
-                delta = -100
-            self.upper_body.position = (self.upper_body.position.x, 300 + delta)
-        pygame.quit()
-
-app = PidApp()
-app.setup()
-app.run()
